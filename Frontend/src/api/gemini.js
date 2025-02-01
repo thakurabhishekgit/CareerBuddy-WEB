c8ec5bfa5b558947372fb9a0534f352198060152
Abhishek Thakur <abhishek.thakur.mainacc@gmail.com>
2025-02-01 13:23:24 +0530

    without api key

--- Frontend/src/api/gemini.js
+++ Frontend/src/api/gemini.js
@@ -5,7 +5,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai"
 
-const apiKey = "AIzaSyAFXOKE8qMD6tECr9A9JT9OMPKFcrQIvp4";
+const apiKey = "";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
